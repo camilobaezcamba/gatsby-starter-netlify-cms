1fa2bdd99683df69f9f00b5f743661f54be6efd1
Camilo Baez <cbaezgracia@gmail.com>
2019-12-03 11:05:08 -0300

    test

---
+++ gatsby-ssr.js
@@ -0,0 +1,10 @@
+const IntlProvider = require('react-intl')
+exports.wrapRootComponent = (Root) => {
+  const locale = 'en';
+  const Wrapper = () => (
+    <IntlProvider locale={locale}>
+        <Root />
+    </IntlProvider>
+  )
+  return Wrapper;
+}
