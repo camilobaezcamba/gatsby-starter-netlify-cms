d4266621e178591f30ad92774d06ae6af689c511
Camilo Baez <cbaezgracia@gmail.com>
2019-12-03 16:57:40 -0300

    test

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,19 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 import { IntlProvider } from 'react-intl'
-import { useIntl } from 'gatsby-plugin-intl'
 
-const AboutPagePreview = ({ entry, widgetFor }) => {
-  console.log(useIntl());
-  return (
-    <IntlProvider locale='es' messages={{'title': 'My title test'}}>
-    <AboutPageTemplate
-      title={entry.getIn(['data', 'title'])}
-      content={widgetFor('body')}
-    />
-    </IntlProvider>  
-  )
-}
+const AboutPagePreview = ({ entry, widgetFor }) => (
+  <IntlProvider locale='es' messages={{'title': 'My title'}}>
+  <AboutPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+  </IntlProvider>  
+)
+
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
