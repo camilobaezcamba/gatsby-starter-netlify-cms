fbac882115bcd7b49e409f643e9e4c33058a35a7
Camilo Baez <cbaezgracia@gmail.com>
2019-12-03 15:42:56 -0300

    test

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+import { Link } from 'gatsby-plugin-intl'
 
 export const IndexPageTemplate = ({
   image,
