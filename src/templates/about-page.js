fb7b34312407e8d3809045ee8706942eff941995
Camilo Baez <cbaezgracia@gmail.com>
2019-12-03 16:18:04 -0300

    test

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -10,7 +10,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <IntlProvider>
+    <IntlProvider locale='es'>
     <section className="section section--gradient">
       <div className="container">
         <div className="columns">
@@ -19,7 +19,7 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
                 {intl.formatMessage({ id: "title" })}
-                {'asdf'}
+                {'bbbbbbb'}
               </h2>
               <PageContent className="content" content={content} />
             </div>
