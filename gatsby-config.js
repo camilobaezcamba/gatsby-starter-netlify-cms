118f122130e38d7d623e4512529b5379d873eb1c
Camilo Baez <cbaezgracia@gmail.com>
2019-12-03 16:02:45 -0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,12 +61,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
@@ -84,6 +78,12 @@ module.exports = {
         defaultLanguage: Object.values(locales).filter(value => value.default)[0].path,
         // option to redirect to `/ko` when connecting `/`
         redirect: false,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
