62e04d0c6c39d571917fa0eeff1ff662c5414796
Camilo Baez <cbaezgracia@gmail.com>
2019-12-10 15:57:22 -0300

    nuevo

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql, Link } from 'gatsby'
+import { useTranslation } from "react-i18next";
 
 export const IndexPageTemplate = ({
   image,
