accd6c7ff089ee279b6bdbffafbe049ee86d661b
camilo <cbaez@vue.gov.py>
2019-12-14 21:21:38 -0300

    test driver page

---
+++ static/img/utils/typography.js
@@ -0,0 +1,38 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  googleFonts: [
+    {
+      name: "Montserrat",
+      styles: ["400", "500", "600", "700"]
+    }
+  ],
+  bodyFontFamily: ["Montserrat", "sans-serif"],
+  bodyColor: "#fff",
+  headerFontFamily: ["Montserrat", "sans-serif"],
+  overrideStyles: ({ options, styles }) => {
+    return {
+      h1: {
+        fontSize: "50px",
+        fontWeight: 600,
+        lineHeight: "61px",
+      },
+      h2: {
+        fontSize: "40px",
+        lineHeight: "49px",
+        fontWeight: 600,
+      },
+      h3: {
+        fontSize: "25px",
+        fontWeight: 600
+      },
+      p: {
+        lineHeight: "34px",
+        fontSize: "20px",
+        fontWeight: 500
+      }
+    }
+  }
+})
+
+export default typography
